f6a7e25379ebbdf146a358f37c6dbb171751863e
Ampinka <51933080+Ampinka@users.noreply.github.com>
2019-09-18 09:58:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,10 +23,10 @@ def callback():
     user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
     userText = decoded['queryResult']['intent']['displayName']
     #sendText(user,userText)
-    if (userText == 'สวัสดี') :
+    if (userText == 'สวีดัส') :
         sendText(user,'สวัสดีค่ะ')
-    elif (userText == 'ทำไรอยู่') :
-        sendText(user,'นอนเล่น')
+    elif (userText == 'ดีจ้ะ') :
+        sendText(user,'สบายดีไหมค่ะ')
     else :
         sendText(user,'ฉันไม่เข้าใจ)
                  
